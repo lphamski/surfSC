4238448221dd4e332df5b0d7275aea5a8f99db67
Leon Pham <lpham10@ucsc.edu>
2019-11-22 21:36:30 -0800

    first commit, lets go

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Forecast from './components/forecast'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Forecast/>
+      <header className="App-header">
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
