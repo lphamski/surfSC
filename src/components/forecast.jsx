1a732d2ece4c888a85e381b1088366c5e955a7fc
Leon Pham <lpham10@ucsc.edu>
2019-12-04 22:38:41 -0800

    initial commit

--- src/components/forecast.jsx
+++ src/components/forecast.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Card, Button, AnchorButton } from '@blueprintjs/core'
 
 function Forecast() {
     return (
-        <div>
-            
-        </div>
+        <React.Fragment>
+            <Card interactive={true}>
+                <h5><a href="/">Card heading</a></h5>
+                <p>Card content</p>
+                <Button icon="refresh">Submit</Button>
+                <AnchorButton text="Click" />
+            </Card>
+        </React.Fragment>
     )
 }
 
