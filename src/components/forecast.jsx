c8fa9050fc52019d17064a8c8eef46e0380dd968
Leon Pham <lpham10@ucsc.edu>
2019-12-04 23:32:21 -0800

    got the surfline API to work

--- src/components/forecast.jsx
+++ src/components/forecast.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card, Button, AnchorButton } from '@blueprintjs/core'
+import axios from 'axios'
+import '../css/forecast.scss'
 
 function Forecast() {
+    //going to store API calls into data below using react hooks
+    const [data, setData] = useState({})
+    
+    useEffect(() => {
+        const forecastData = async () => {
+            const result = await axios(
+                'https://services.surfline.com/kbyg/spots/forecasts/wave?spotId=5842041f4e65fad6a7708807',
+            )
+            console.log(result)
+            setData(result.data)
+        }
+        forecastData()
+        
+    }, [])
+
+
+    // might add animations to this below
+    const handleClick = () => {}
+
     return (
         <React.Fragment>
-            <Card interactive={true}>
-                <h5><a href="/">Card heading</a></h5>
-                <p>Card content</p>
+            <Card className="Forecast-card" onClick={handleClick} interactive={true}>
+                <h5>Santa Cruz Surf</h5>
+                <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.t</p>
                 <Button icon="refresh">Submit</Button>
                 <AnchorButton text="Click" />
             </Card>
