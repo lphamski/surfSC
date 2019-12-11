151cb3db0d80bbaedb851ff1ad95e3cb22d56385
Leon Pham <lpham10@ucsc.edu>
2019-12-10 23:32:06 -0800

    usestate doesn't allow big nested objects

--- src/components/forecast.jsx
+++ src/components/forecast.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Card, Button, AnchorButton } from '@blueprintjs/core';
 import MagicSeaWeed from './magicSeaWeed';
 import Surfline from './surfline';
 
@@ -13,11 +12,7 @@ function Forecast() {
 
     return (
         <React.Fragment>
-            <Card className="Forecast-card" onClick={handleClick} interactive={true}>
-                <Surfline/>
-                <Button icon="refresh">Submit</Button>
-                <AnchorButton text="Click" />
-            </Card>
+            <Surfline/>
         </React.Fragment>
     );
 }
--- src/components/surfline.jsx
+++ src/components/surfline.jsx
@@ -1,27 +1,57 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from "react";
+import { Card, Button, AnchorButton } from "@blueprintjs/core";
+import "../css/surfline.scss";
 
 function Surfline() {
-    const [surflineData, setSurflineData] = useState({});
-    const [dataType, setDataType] = useState('wave')
+  // url query variable
+  const [dataType, setDataType] = useState("wave");
+  // units variable
+  const [surflineUnits, setSurflineUnits] = useState({});
+  // array of wave data 144 objects; 6 days 1 entry per hour
+  const [waveData, setWaveData] = useState([]);
+  // long lat for location
+  const [longLat, setLongLat] = useState({});
 
-
+  useEffect(() => {
     const forecastData = async () => {
-        var url = 'https://services.surfline.com/kbyg/spots/forecasts/wave?spotId=5842041f4e65fad6a7708807'
-        const response = await fetch(url);
-        const data = await response.json();
-        setSurflineData(data);
-    }
+      var url =
+        "https://services.surfline.com/kbyg/spots/forecasts/" +
+        dataType +
+        "?spotId=5842041f4e65fad6a7708807";
+      const response = await fetch(url);
+      const data = await response.json();
+      setSurflineUnits(data.associated.units);
+      setWaveData(data.data.wave);
+      setLongLat(data.associated.location);
+      console.log(data);
+    };
+    forecastData();
+  }, [dataType]);
 
-    useEffect( () => {
-        forecastData();
-    }, []);
-    console.log(surflineData.associated.units)
+  console.log(waveData);
+  console.log(surflineUnits);
 
-    return (
-            <p>Longitude: {typeof surflineData}</p>
+  const getOkay = arr => {
+    
+      arr.map(wave => (
+        <div className="station" key={wave}>
+          {wave}
+        </div>
+      ));
+  };
 
-    );
+  return (
+    <Card className="Surfline-card">
+      <h1>
+        <strong>Pleasure Point</strong>
+      </h1>
+      <p>
+        <strong>Longitude:</strong> {longLat.lon} <strong>Latitude:</strong>{" "}
+        {longLat.lat}
+      </p>
+      <span>{getOkay}</span>
+    </Card>
+  );
 }
 
 export default Surfline;
